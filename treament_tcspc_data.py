e35c194eaa6515461bd2c5f20e50d18796b778d8
Habakuk <habakuk.conrado@gmail.com>
2026-05-04 17:32:28 -0300

    treating independent data

---
+++ treament_tcspc_data.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def treat_npy_array(nparr):
+    """Return a list of arrays, where each array is the difference between the current and previous array in nparr. The first array is returned as is."""
+    data = []
+    xs = np.array(nparr[0][0])
+    ys = np.array(nparr[0][1])
+    # print(nparr[0][1])
+    # xs, ys = [], []
+    for idx in range(len(nparr)):
+        ys = nparr[idx][1] if idx == 0 else (nparr[idx][1]-nparr[idx-1][1])
+        data.append(np.column_stack((xs,ys)))
+    return data 
